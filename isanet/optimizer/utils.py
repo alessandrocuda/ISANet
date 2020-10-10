b89aff49e1cd9d31900c4918fb0ca19ce3dca582
Giulia Volpi <38424440+GiuliaVolpi@users.noreply.github.com>
2020-10-11 00:06:55 +0200

    Add docs in utills

--- isanet/optimizer/utils.py
+++ isanet/optimizer/utils.py
@@ -1,19 +1,66 @@
 import numpy as np
 
 def l_norm(l_v):
-    # compute the norm of a list of numpy array
+    """Computes the norm of a list of numpy array.
+
+    Parameters
+    ----------
+    l_v : array-like
+
+    Returns
+    -------
+    float
+        The norm of l_v.
+    """
     return np.sqrt(np.sum([np.sum(np.square(l_v[i])) for i in range(0, len(l_v))]))
 
 def l_scalar_product(l_v, l_w):
-    # compute the scalar product between two list of numpy array
+    """Computes the scalar product between two list of numpy array.
+
+    Parameters
+    ----------
+    l_v : array-like
+    l_w : array-like
+
+    Returns
+    -------
+    float
+        The scalar product between l_v and l_w.
+
+    """
+
     return np.sum([np.sum(np.multiply(l_v[i], l_w[i])) for i in range(0, len(l_v))])
 
 def make_vector(l_v):
-    #take a list of numpy array and return a collum vector
+    """Takes a list of numpy array and returns a column vector.
+
+    Parameters
+    ----------
+    l_v : array-like
+
+    Returns
+    -------
+    array
+        Array with dimensions (n, 1).
+    """
+
     row_vector = [l_v[l].flatten() for l in range(len(l_v))]
     return np.concatenate(row_vector).reshape(-1, 1)
 
 def restore_w_to_model(model, w):
+    """Takes an array of weights and transforms them into a list 
+       of matrices with dimensions taken from the model passed.
+
+    Parameters
+    ----------
+    model : isanet.model.MLP
+            The Multilayer Perceptron object.
+    w : array
+
+    Returns
+    -------
+        array-like
+    """
     start = 0
     weights = [0]*model.n_layers
     for i in range(model.n_layers):
